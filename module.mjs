e2ff70cc2f04d78d5d95ace19d27cbc85c1d72ac
Deyzeria <vikbar199@gmail.com>
2025-07-06 12:08:53 +0300

    Brought pre-v13 styles

--- module.mjs
+++ module.mjs
@@ -7,7 +7,7 @@ globalThis.templatePath = path => path ? systemPath(`templates/${path}.hbs`) : s
 
 Hooks.once("init", () => {
     globalThis.dishonored = {
-        tracker: new DishonoredMomentumTracker(),
+        tracker: new DishonoredTracker(),
     };
 
     registerSettings();
@@ -22,7 +22,7 @@ Hooks.once("ready", async () => {
 function registerSocketEvents() {
     game.socket.on(`module.${MODULE_ID}`, event => {
         if (event.type === "setCounter" && game.user.isGM) {
-            DishonoredMomentumTracker.setCounter(event.payload.value, event.payload.type);
+            DishonoredTracker.setCounter(event.payload.value, event.payload.type);
         }
 
         if (event.type === "updateCounter") {
@@ -31,150 +31,197 @@ function registerSocketEvents() {
     });
 }
 
-class DishonoredMomentumTracker
-    extends HandlebarsApplicationMixin(ApplicationV2) {
+class DishonoredTracker extends Application {
 
-    static DEFAULT_OPTIONS = {
-        actions: {
-            decrement: this._onDecrement,
-            increment: this._onIncrement,
-        },
-        classes: ["dishonored"],
-        form: {
-            closeOnSubmit: false,
-            submitOnChange: true,
-            handler: DishonoredMomentumTracker._onSubmit,
-        },
-        tag: "form",
-        window: {
-            frame: false,
-            resizable: false,
-        },
-    };
+	static get defaultOptions() {
+		return foundry.utils.mergeObject(super.defaultOptions, {
+			template: templatePath("momentum-tracker"),
+			popOut: false,
+			resizable: false,
+		});
+	}
 
+	get canEditChaos() {
+		const chaosEditRole = game.settings.get(MODULE_ID, "chaosPermissionLevel");
+		return game.user.isGM || game.user.hasRole(chaosEditRole);
+	}
 
-    static PARTS = {
-        form: {
-            template: templatePath("momentum-tracker"),
-            classes: ["momentum-tracker"],
-        },
-    };
+	get canEditMomentum() {
+		const momentumEditRole = game.settings.get(MODULE_ID, "momentumPermissionLevel");
+		return game.user.isGM || game.user.hasRole(momentumEditRole);
+	}
 
+	activateListeners(html) {
+		html.find("#dishonored-chaos-track-increase").click(event => {
+			event.preventDefault();
+			DishonoredTracker.changeCounter(1, "chaos");
+		});
 
-    /**
-     * Change the counter of (type) by (value)
-     * @param diff  How much to change the counter
-     * @param type  Type of counter, "momentum" or "chaos"
-     */
-    static async changeCounter(diff, type) {
-        this.checkCounterUpdate(diff, type);
+		html.find("#dishonored-chaos-track-decrease").click(event => {
+			event.preventDefault();
+			DishonoredTracker.changeCounter(-1, "chaos");
+		});
 
-        const newValue = game.settings.get(MODULE_ID, type) + diff;
-        await DishonoredMomentumTracker.setCounter(newValue, type);
-    }
+		// We want it so that we unfocus if the enter key is pressed, we do this
+		// by recording the keycode 13 and bluring.
+		html.find("#dishonored-track-chaos").keydown(event => {
+			if (event.keyCode === 13) {
+				event.preventDefault();
+				html.find("#dishonored-track-chaos").blur();
+			}
+		});
 
+		// This is what is fired when the chaos tracker text box is edited.
+		html.find("#dishonored-track-chaos").change(event => {
+			event.preventDefault();
 
-    // Check user entry. Rerender if error is detected to reset to the correct value
-    static checkCounterUpdate(value, type) {
-        const updateError = {
-            counter: "Dishonored | Error updating Counter: Invalid Counter Type",
-            value: "Dishonored | Error updating Counter: Invalid Value Type",
-        };
+			const value = document.getElementById("dishonored-track-chaos").value ?? "";
 
-        if (type !== "chaos" && type !== "momentum") {
-            ui.notifications.error("Error updating Counter: Invalid Counter Type");
-            dishonored.tracker.render(true);
-            throw updateError.counter;
-        }
+			// Ignore empty values and just re-render
+			if (value === "") return dishonored.tracker.render(true);
 
-        if (value !== 0 && !value || Number.isNaN(value)) {
-            ui.notifications.error("Error updating Counter: Invalid Value Type");
-            dishonored.tracker.render(true);
-            throw updateError.value;
-        }
-    }
+			// Handle +/- adjustments
+			const plusMinusMatch = /([+-]{1})\s*(\d+)/;
+			const plusMinusMatched = value.match(plusMinusMatch);
+			if (plusMinusMatched) {
+				const sign = plusMinusMatched[1];
+				const intValue = Number.parseInt(plusMinusMatched[2]);
 
+				const diff = sign === "-" ? -intValue : intValue;
 
-    /**
-     * Set the counter of (type) to (value)
-     * @param value Value to set counter to
-     * @param type  Type of counter, "momentum" or "chaos"
-     */
-    static async setCounter(value, type) {
-        DishonoredMomentumTracker.checkCounterUpdate(value, type);
+				return DishonoredTracker.changeCounter(diff, "chaos");
+			}
 
-        if (!game.user.isGM) {
-            game.socket.emit(`module.${MODULE_ID}`, {
-                type: "setCounter",
-                payload: { value, type },
-            });
-            return;
-        }
+			// Now make sure it's a number and assign if so
+			const intValue = Number.parseInt(value);
 
-        value = Number.parseInt(value);
+			if (isNaN(intValue)) {
+				// Ignore bad values and re-render
+				return dishonored.tracker.render(true);
+			}
+			else {
+				return DishonoredTracker.setCounter(intValue, "chaos");
+			}
+		});
 
-        value = Math.max(0, value);
+		html.find("[id^=\"dishonored-momentum-tracker\"]").click(ev => {
+			if (!this.canEditMomentum) return;
 
-        if (type === "momentum") {
-            value = Math.min(6, value);
-        }
-        else {
-            value = Math.min(99, value);
-        }
+			let captureObject = $(ev.currentTarget)[0];
+			let newTotal = captureObject.id.replace(/\D/g, "");
 
-        await game.settings.set(MODULE_ID, type, value);
+			let newMomentum = Number.parseInt(newTotal);
+			const currentMomentum = game.settings.get(MODULE_ID, "momentum");
 
-        dishonored.tracker.render(true);
+			if (newMomentum === currentMomentum) {
+				newMomentum--;
+			}
 
-        // Emit socket event for users to rerender their counters
-        game.socket.emit(`module.${MODULE_ID}`, { type: "updateCounter" });
-    }
+			return DishonoredTracker.setCounter(newMomentum, "momentum");
+		});
+	}
 
+	/**
+	 * Change the counter of (type) by (value)
+	 * @param diff How much to change the counter
+	 * @param type  Type of counter, "momentum" or "chaos"
+	 */
+	static async changeCounter(diff, type) {
+		this.checkCounterUpdate(diff, type);
 
-    static async _onDecrement(event, html) {
-        const { resource } = event.target.parentElement?.dataset ?? undefined;
+		let value = game.settings.get(MODULE_ID, type);
+		if (value + diff > 6 && type === "momentum") {
+			await DishonoredTracker.setCounter(6, type);
+		}
+		else if (value + diff > 99 && type === "chaos") {
+			await DishonoredTracker.setCounter(99, type);
+		}
+		else if (value + diff < 0) {
+			await DishonoredTracker.setCounter(0, type);
+		}
+		else {
+			value += diff;
+			await DishonoredTracker.setCounter(value, type);
+		}
+	}
 
-        if (resource) DishonoredMomentumTracker.changeCounter(-1, resource);
-    }
+	// Check user entry. Rerender if error is detected to reset to the correct value
+	static checkCounterUpdate(value, type) {
+		const updateError = {
+			counter: "Dishonored | Error updating Counter: Invalid Counter Type",
+			value: "Dishonored | Error updating Counter: Invalid Value Type",
+		};
 
+		if (type !== "chaos" && type !== "momentum") {
+			ui.notifications.error("Error updating Counter: Invalid Counter Type");
+			dishonored.tracker.render(true);
+			throw updateError.counter;
+		}
 
-    static async _onIncrement(event, html) {
-        const { resource } = event.target.parentElement?.dataset ?? undefined;
+		if (Number.isNaN(value)) {
+			ui.notifications.error("Error updating Counter: Invalid Value Type");
+			dishonored.tracker.render(true);
+			throw updateError.value;
+		}
+	}
 
-        if (resource) DishonoredMomentumTracker.changeCounter(1, resource);
-    }
+	async getData() {
+		const data = super.getData();
 
+		data.canEditChaos = this.canEditChaos;
+		data.canEditMomentum = this.canEditMomentum;
 
-    static async _onSubmit(event, form, formData) {
-        const { resource } = event.target.dataset ?? undefined;
-        const value = event.target.value;
-        DishonoredMomentumTracker.setCounter(value, resource);
-    }
+		data.chaos = game.settings.get(MODULE_ID, "chaos");
+		data.momentum = game.settings.get(MODULE_ID, "momentum");
 
+		// Create LUT which specifies wether a momentum segment should be shown
+		// as enabled or not
+		//
+		data.momentumEnabled = {};
 
-    get canEditChaos() {
-        const chaosEditRole = game.settings.get(MODULE_ID, "chaosPermissionLevel");
-        return game.user.isGM || game.user.hasRole(chaosEditRole);
-    }
+		for (let i = 0; i < 6; i++) {
+			data.momentumEnabled[i + 1] = i < data.momentum;
+		}
 
+		return data;
+	}
 
-    get canEditMomentum() {
-        const momentumEditRole = game.settings.get(MODULE_ID, "momentumPermissionLevel");
-        return game.user.isGM || game.user.hasRole(momentumEditRole);
-    }
+	/**
+	 * Set the counter of (type) to (value)
+	 * @param value Value to set counter to
+	 * @param type  Type of counter, "momentum" or "chaos"
+	 */
+	static async setCounter(value, type) {
+		DishonoredTracker.checkCounterUpdate(value, type);
 
+		value = Math.round(value);
 
-    async _prepareContext(options = {}) {
-        const context = await super._prepareContext(options);
+		if (!game.user.isGM) {
+			game.socket.emit(`module.${MODULE_ID}`, {
+				type: "setCounter",
+				payload: {value, type},
+			});
+			return;
+		}
 
-        context.canEditChaos = this.canEditChaos;
-        context.canEditMomentum = this.canEditMomentum;
+		if (value > 6 && type === "momentum") {
+			await game.settings.set(MODULE_ID, type, 6);
+		}
+		if (value > 99 && type === "chaos") {
+			await game.settings.set(MODULE_ID, type, 99);
+		}
+		else if (value < 0) {
+			await game.settings.set(MODULE_ID, type, 0);
+		}
+		else {
+			await game.settings.set(MODULE_ID, type, value);
+		}
 
-        context.chaos = game.settings.get(MODULE_ID, "chaos");
-        context.momentum = game.settings.get(MODULE_ID, "momentum");
+		dishonored.tracker.render(true);
 
-        return context;
-    }
+		// Emit socket event for users to rerender their counters
+		game.socket.emit(`module.${MODULE_ID}`, {type: "updateCounter"});
+	}
 }
 
 function registerSettings() {
